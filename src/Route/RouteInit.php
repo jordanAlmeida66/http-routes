6fb0b314e402976a30d8b34eadc452406e912e60
jordanAlmeida66 <jordansantos4254@gmail.com>
2022-05-23 08:50:00 -0300

    CREATE RouteInit Classe responsável por gerenciar ações de um rota em específico

---
+++ src/Route/RouteInit.php
@@ -0,0 +1,108 @@
+<?php
+namespace HttpRoutes\Route;
+
+use HttpRoutes\Exception\BootstrapException;
+
+class RouteInit
+{
+  private $controller = [];
+  private $callback;
+  private $arguments = [];
+  private $middleware = [];
+  private $function = [];
+
+  public function setController($controller, $action)
+  {
+    $this->controller['controller'] = $controller;
+    $this->controller['action'] = $action;
+  }
+
+  public function setFunction(array $function)
+  {
+    foreach ($function as $key => $value) {
+      $this->function[$key] = $value;
+    }
+  }
+
+  public function setCallback(callable $callback)
+  {
+    $this->callback = $callback;
+  }
+
+  /**
+   * Execulta os middlewares da rota
+   * 
+   * @param array $middlewares
+   * @return void
+   */
+  public function setMiddleware(array $middleware)
+  {
+    $this->middleware = $middleware;
+  }
+
+  public function setArguments(array $arguments)
+  {
+    $this->arguments = $arguments;
+  }
+
+  public function run()
+  {
+    if (!empty($this->middleware)) {
+      $this->execMiddleware($this->middleware);
+    }
+
+    if (!empty($this->controller)) {
+      $controller = $this->controller['controller'];
+      $action = $this->controller['action'];
+
+      $this->execController($controller, $action);
+    } else if (!is_null($this->callback)) {
+      $this->execCallback($this->callback);
+    } else {
+      throw new BootstrapException("Nenhum Controller ou callback encontrado");
+    }
+  }
+
+  private function execMiddleware(array $middlewares): void
+  {  
+    foreach ($middlewares as $key => $middleware) {
+
+      $class = key($middleware);
+      $action = current($middleware);
+      $m = new $class;
+
+      $r = $m->$action();
+
+      is_null($r) ? '' : 
+      (is_array($r) ? die(json_encode($r)) : die($r));
+
+      //echo "passou na validação do middleware {$class} $action<br>";
+    }  
+  }
+
+  private function execCallback($callback) : void
+  {
+    $response = !empty($this->arguments) ? $callback($this->function, $arguments) : $callback($this->function);
+
+    is_null($response) ? die() :'';
+
+    if (is_array($response)) {
+      header('Content-Type: application/json');
+      die(json_encode($response));
+    }
+       
+    die($response); 
+  }
+
+  private function execController($controller, $action) : void
+  {
+    $c = new $controller($this->function);
+    $response = isset($this->arguments) ? $c->$action($this->arguments): $c->$action();
+
+    is_null($response) ? die() :'';
+
+    is_array($response) ? die(json_encode($response)) : '';
+       
+    die($response); 
+  } 
+}
